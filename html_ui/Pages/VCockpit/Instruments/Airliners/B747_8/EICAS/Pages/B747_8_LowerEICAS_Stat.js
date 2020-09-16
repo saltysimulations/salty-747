694c2ac646ccf04f676c151a11b9205bc1a9cfd9
Ninjo <ninjomcs@gmail.com>
2020-09-16 23:36:25 +0200

    Added STAT page UTC time and date

--- html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/EICAS/Pages/B747_8_LowerEICAS_Stat.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/EICAS/Pages/B747_8_LowerEICAS_Stat.js
@@ -12,13 +12,41 @@ var B747_8_LowerEICAS_Stat;
         }
         init() {
             this.isInitialised = true;
+            this.months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+            this.date = document.querySelector("#date"),
+            this.utcTime = document.querySelector("#utctime");
+            this.elapsedTime = document.querySelector("#time");
         }
         update(_deltaTime) {
             if (!this.isInitialised) {
                 return;
             }
+            this.updateClock();
+        }
+        updateClock() {
+            var utc = new Date();
+            if (utc.getUTCHours() <= 9) {
+                var utcHours = "0" + utc.getUTCHours();
+            } else {
+                var utcHours = utc.getUTCHours();
+            }
+            if (utc.getUTCMinutes() <= 9) {
+                var utcMinutes = "0" + utc.getUTCMinutes();
+            } else {
+                var utcMinutes = utc.getUTCMinutes();
+            }
+            if (utc.getUTCSeconds() <= 9) {
+                var utcSeconds = "0" + utc.getUTCSeconds();
+            } else {
+                var utcSeconds = utc.getUTCSeconds();
+            }
+            var combinedUTC = utcHours + ":" + utcMinutes + ":" + utcSeconds;
+            var combinedDate = utc.getUTCDate() + " " + this.months[utc.getUTCMonth()] + " " + utc.getUTCFullYear();
+            this.utcTime.textContent = combinedUTC;
+            this.date.textContent = combinedDate;
         }
     }
+        
     B747_8_LowerEICAS_Stat.Display = Display;
 })(B747_8_LowerEICAS_Stat || (B747_8_LowerEICAS_Stat = {}));
 customElements.define("b747-8-lower-eicas-stat", B747_8_LowerEICAS_Stat.Display);
