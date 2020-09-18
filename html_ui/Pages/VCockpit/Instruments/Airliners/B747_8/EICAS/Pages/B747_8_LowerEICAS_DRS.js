5234fd9eb6426057dd3c22bd995ef30ecd165ac7
Ninjo <ninjomcs@gmail.com>
2020-09-18 23:07:06 +0200

    Added functionality to DRS page
    DRS page now functions correctly when a door is opened / closed.

--- html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/EICAS/Pages/B747_8_LowerEICAS_DRS.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/EICAS/Pages/B747_8_LowerEICAS_DRS.js
@@ -12,13 +12,45 @@ var B747_8_LowerEICAS_DRS;
         }
         init() {
             this.isInitialised = true;
-            this.entry1Status = document.querySelector("#entry1-status");
+
+            // Status text, eg. "A"
+            this.entry1LStatus = document.querySelector("#entry1-status");
+            this.entry5RStatus = document.querySelector("#entry5-status");
+
+            // Rectangles that appear when door is open
+            this.entry1LRect = document.querySelector("#entry1-rect");
+            this.fwdCargoRect = document.querySelector("#fwdcargo-rect");
+            this.entry5RRect = document.querySelector("#entry5-rect");
         }
         update(_deltaTime) {
             if (!this.isInitialised) {
                 return;
             }
-            
+
+            // SimVars for checking if door is open, in percentage opened
+            var entry1LOpen = SimVar.GetSimVarValue("INTERACTIVE POINT OPEN:10", "percent");
+            var fwdCargoOpen = SimVar.GetSimVarValue("INTERACTIVE POINT OPEN:12", "percent");
+            var entry5ROpen = SimVar.GetSimVarValue("INTERACTIVE POINT OPEN:1", "percent");
+
+            if (entry1LOpen >= 40) {
+                this.entry1LStatus.textContent = "";
+                this.entry1LRect.style.visibility = "visible";
+            } else {
+                this.entry1LStatus.textContent = "A";
+                this.entry1LRect.style.visibility = "hidden";
+            }
+            if (fwdCargoOpen >= 40) {
+                this.fwdCargoRect.style.visibility = "visible";
+            } else {
+                this.fwdCargoRect.style.visibility = "hidden";
+            }
+            if (entry5ROpen >= 40) {
+                this.entry5RStatus.textContent = "";
+                this.entry5RRect.style.visibility = "visible";
+            } else {
+                this.entry5RStatus.textContent = "A";
+                this.entry5RRect.style.visibility = "hidden";
+            }
         }
     }
     B747_8_LowerEICAS_DRS.Display = Display;
