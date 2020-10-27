3ec42b2b68f3cf8a69fe36ebfe254a29473ad5eb
747-4EVER <747-4EVER@users.noreply.github.com>
2020-10-27 18:48:38 +0000

    Improved VNAV CRZ Page

--- html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/B747_8_FMC_VNAVPage.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/B747_8_FMC_VNAVPage.js
@@ -1,9 +1,16 @@
 class B747_8_FMC_VNAVPage {
     static ShowPage1(fmc) {
         fmc.clearDisplay();
+        B747_8_FMC_VNAVPage._timer = 0;
+        fmc.pageUpdate = () => {
+            B747_8_FMC_VNAVPage._timer++;
+            if (B747_8_FMC_VNAVPage._timer >= 100) {
+                B747_8_FMC_VNAVPage.ShowPage1(fmc);
+            }
+        };
         let crzAltCell = "□□□□□";
         if (fmc.cruiseFlightLevel) {
-            crzAltCell = fmc.cruiseFlightLevel + "FL";
+            crzAltCell = "FL" + fmc.cruiseFlightLevel;
         }
         fmc.onLeftInput[0] = () => {
             let value = fmc.inOut;
@@ -43,10 +50,32 @@ class B747_8_FMC_VNAVPage {
     }
     static ShowPage2(fmc) {
         fmc.clearDisplay();
+        B747_8_FMC_VNAVPage._timer = 0;
+        fmc.pageUpdate = () => {
+            B747_8_FMC_VNAVPage._timer++;
+            if (B747_8_FMC_VNAVPage._timer >= 100) {
+                B747_8_FMC_VNAVPage.ShowPage2(fmc);
+            }
+        };        
+        let crzPageTitle = "ACT ECON CRZ"
+        let crzSpeedMode = "ECON SPD"
+        if (SimVar.GetSimVarValue("L:AP_SPEED_INTERVENTION_ACTIVE", "number") == 1) {
+            crzPageTitle = "ACT MCP SPD CRZ"
+            crzSpeedMode = "SEL SPD"
+        }
+        let crzSpeedCell = ""
+        let machMode = Simplane.getAutoPilotMachModeActive();
+        if (machMode) {
+            let crzMachNo = Simplane.getAutoPilotMachHoldValue().toFixed(3);
+            var radixPos = crzMachNo.indexOf('.');
+            crzSpeedCell = crzMachNo.slice(radixPos);
+        } else {
+            crzSpeedCell = Simplane.getAutoPilotAirspeedHoldValue().toFixed(0);
+        }
         let crzAltCell = "□□□□□";
         if (fmc.cruiseFlightLevel) {
-            crzAltCell = fmc.cruiseFlightLevel + "FL";
-        }
+            crzAltCell = "FL" + fmc.cruiseFlightLevel;
+        }  
         fmc.onRightInput[0] = () => {
             let value = fmc.inOut;
             fmc.clearUserInput();
@@ -59,26 +88,39 @@ class B747_8_FMC_VNAVPage {
         if (isFinite(n1Value)) {
             n1Cell = n1Value.toFixed(1) + "%";
         }
+
+        let weightIndex = SimVar.GetSimVarValue("TOTAL WEIGHT", "pounds") - 608000;
+        let maxFltLevel = 431
+        if (weightIndex >= 0){
+            maxFltLevel = 431 - (weightIndex / 362000 * 110)
+        }
         fmc.setTemplate([
-            ["CRZ", "2", "3"],
+            [crzPageTitle, "2", "3"],
             ["CRZ ALT", "STEP TO"],
             [crzAltCell],
-            ["ECON SPD", "AT"],
-            [],
+            [crzSpeedMode, "AT"],
+            [crzSpeedCell],
             ["N1"],
             [n1Cell],
-            ["STEP", "RECMD", "OPT MAX"],
+            ["STEP", "RECMD", "OPT\xa0\xa0\xa0MAX"],
+            ["RVSM", "", "\xa0\xa0\xa0\xa0\xa0\xa0" + "FL" + maxFltLevel.toFixed(0)],
+            ["__FMCSEPARATOR"],
+            ["", "ENG OUT>"],
             [],
-            ["", "1X @ TOD"],
-            ["", "OFF"],
-            ["PAUSE @ TOD"],
-            ["OFF", "<LRC"]
+            ["<RTA PROGRESS", "LRC>"]
         ]);
         fmc.onPrevPage = () => { B747_8_FMC_VNAVPage.ShowPage1(fmc); };
         fmc.onNextPage = () => { B747_8_FMC_VNAVPage.ShowPage3(fmc); };
     }
     static ShowPage3(fmc) {
         fmc.clearDisplay();
+        B747_8_FMC_VNAVPage._timer = 0;
+        fmc.pageUpdate = () => {
+            B747_8_FMC_VNAVPage._timer++;
+            if (B747_8_FMC_VNAVPage._timer >= 100) {
+                B747_8_FMC_VNAVPage.ShowPage3(fmc);
+            }
+        };
         let speedTransCell = "---";
         let speed = fmc.getDesManagedSpeed();
         if (isFinite(speed)) {
