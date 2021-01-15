4e796e7bf30f73cadb5f2814690f47e185cce03f
747-4EVER <747-4EVER@users.noreply.github.com>
2021-01-15 23:58:45 +0000

    Tweaked offset to perfect center

--- html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/MFD/Svg/SvgWaypointElement.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/MFD/Svg/SvgWaypointElement.js
@@ -225,7 +225,7 @@ class SvgWaypointElement extends SvgMapElement {
                 this._lastY = this.y;
                 let x = (this.x - map.config.waypointIconSize * 0.5 * (this.minimize ? 0.5 : 1));
                 let y = (this.y - map.config.waypointIconSize * 0.5 * (this.minimize ? 0.5 : 1));
-                this.svgElement.setAttribute("x", x - 8 + "");
+                this.svgElement.setAttribute("x", x - 7 + "");
                 this.svgElement.setAttribute("y", y + "");
                 if (this.source instanceof AirportInfo) {
                     let a = this.source.longestRunwayDirection;
