11310b89435705f1df31663f3f542af67288d649
boufogre <73106379+boufogre@users.noreply.github.com>
2021-02-19 18:26:34 +0100

    Many changes
    Many changes

---
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/B747_8_FMC_ThrustLimPage.js
@@ -0,0 +1,83 @@
+class FMCThrustLimPage {
+    static ShowPage1(fmc) {
+        fmc.clearDisplay();
+        let selectedTempCell = fmc.getThrustTakeOffTemp() + "°";
+		/*if (isNaN(selectedTempCell)) {
+			selectedTempCell = "--";
+		}
+		else {
+			selectedTempCell = fmc.getThrustTakeOffTemp ();
+		}*/
+        fmc.onLeftInput[0] = () => {
+            let value = fmc.inOut;
+            fmc.clearUserInput();
+            if (fmc.setThrustTakeOffTemp(value)) {
+                FMCThrustLimPage.ShowPage1(fmc);
+            }
+        };
+        let toN1Cell = fmc.getThrustTakeOffLimit().toFixed(1) + "%";
+        let oatValue = SimVar.GetSimVarValue("AMBIENT TEMPERATURE", "celsius");
+        let oatCell = oatValue.toFixed(0) + "°C";
+        let thrustTOMode = fmc.getThrustTakeOffMode();
+        let thrustClimbMode = fmc.getThrustCLBMode();
+        fmc.onLeftInput[1] = () => {
+            fmc.setThrustTakeOffMode(0);
+            fmc.setThrustCLBMode(0);
+            FMCThrustLimPage.ShowPage1(fmc);
+        };
+        fmc.onLeftInput[2] = () => {
+			if (selectedTempCell === "--"){
+				fmc.setThrustTakeOffMode(1);
+				fmc.setThrustCLBMode(1);
+				FMCThrustLimPage.ShowPage1(fmc);
+			}
+			else {
+				fmc.setThrustTakeOffMode(1);
+				fmc.setThrustCLBMode(1);
+				FMCThrustLimPage.ShowPage1(fmc);
+			}
+        };
+        fmc.onLeftInput[3] = () => {
+            if (selectedTempCell === "--"){
+				fmc.setThrustTakeOffMode(2);
+				fmc.setThrustCLBMode(2);
+				FMCThrustLimPage.ShowPage1(fmc);
+			}
+			else {
+				fmc.setThrustTakeOffMode(2);
+				fmc.setThrustCLBMode(2);
+				FMCThrustLimPage.ShowPage1(fmc);
+			}
+        };
+        fmc.onRightInput[1] = () => {
+            fmc.setThrustCLBMode(0);
+            FMCThrustLimPage.ShowPage1(fmc);
+        };
+        fmc.onRightInput[2] = () => {
+            fmc.setThrustCLBMode(1);
+            FMCThrustLimPage.ShowPage1(fmc);
+        };
+        fmc.onRightInput[3] = () => {
+            fmc.setThrustCLBMode(2);
+            FMCThrustLimPage.ShowPage1(fmc);
+        };
+        fmc.setTemplate([
+            ["THRUST LIM"],
+            ["SEL", "TO N1", "OAT"],
+            [selectedTempCell, toN1Cell, oatCell],
+            [""],
+            ["\<TO" + (thrustTOMode === 0 ? " <SEL>" : ""), (thrustClimbMode === 0 ? "<ARM> " : "") + "CLB>"],
+            ["TO  1"],
+            ["\<-10%" + (thrustTOMode === 1 ? " <SEL>" : ""), (thrustClimbMode === 1 ? "<ARM> " : "") + "CLB 1>"],
+            ["TO  2"],
+            ["\<-20%" + (thrustTOMode === 2 ? " <SEL> " : ""), (thrustClimbMode === 2 ? "<ARM> " : "") + "CLB 2>" ],
+            [""],
+            [""],
+            ["__FMCSEPARATOR"],
+            ["\<INDEX", "TAKEOFF>"]
+        ]);
+        fmc.onLeftInput[5] = () => { B747_8_FMC_InitRefIndexPage.ShowPage1(fmc); };
+        fmc.onRightInput[5] = () => { FMCTakeOffPage.ShowPage1(fmc); };
+    }
+}
+//# sourceMappingURL=B747_8_FMC_ThrustLimPage.js.map
