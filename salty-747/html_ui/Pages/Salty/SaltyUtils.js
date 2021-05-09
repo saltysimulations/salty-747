202d38e2cb3859385e250400fbe3b1451d766225
crGowen <39204053+crGowen@users.noreply.github.com>
2021-05-09 23:25:15 +0100

    Util to set attributes, should improve readbility

---
+++ salty-747/html_ui/Pages/Salty/SaltyUtils.js
@@ -0,0 +1,13 @@
+const SaltyUtils =  {
+
+    setAttributes = (_elem, _attributes) => {
+
+        attributes.forEach(attrib => {
+            if (attrib.length === 2) {
+                _elem.setAttribute(attrib[0], attrib[1]);
+            }
+        });
+    }
+
+    
+};
