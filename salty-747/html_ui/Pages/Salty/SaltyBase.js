b644617d4bc67134fa44ea2d1856ba1f6cb63edc
crGowen <39204053+crGowen@users.noreply.github.com>
2021-05-11 17:46:03 +0100

    2/3 knobs working

--- salty-747/html_ui/Pages/Salty/SaltyBase.js
+++ salty-747/html_ui/Pages/Salty/SaltyBase.js
@@ -2,12 +2,15 @@ class SaltyBase {
     constructor() {
         this.irs = new SaltyIRS();
 	this.pilots = new SaltyPilots();
+    this.jettison = new SaltyJettison();
     }
     init() {
         this.irs.init();
 	this.pilots.init();
+    this.jettison.init();
     }
     update() {
         this.irs.update();
+        this.jettison.update();
     }
 }
