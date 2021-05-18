3e78ed3f30cac09e19a38b9066b6bc2098e19655
Ninjo <ninjomcs@gmail.com>
2021-05-18 17:58:54 +0200

    formatting 2

--- salty-747/html_ui/Pages/Salty/SaltyBase.js
+++ salty-747/html_ui/Pages/Salty/SaltyBase.js
@@ -1,13 +1,13 @@
 class SaltyBase {
     constructor() {
         this.irs = new SaltyIRS();
-	this.pilots = new SaltyPilots();
-    this.jettison = new SaltyJettison();
+        this.pilots = new SaltyPilots();
+        this.jettison = new SaltyJettison();
     }
     init() {
         this.irs.init();
-	this.pilots.init();
-    this.jettison.init();
+        this.pilots.init();
+        this.jettison.init();
     }
     update() {
         this.irs.update();
