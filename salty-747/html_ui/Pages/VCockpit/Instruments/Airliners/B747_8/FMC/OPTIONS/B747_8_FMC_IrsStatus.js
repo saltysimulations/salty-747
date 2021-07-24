2966ebf6026ca6ad78807fc879474d150acd6d9d
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2021-07-24 20:20:25 +0800

    Update B747_8_FMC_IrsStatus.js
    Add IRS/CHKL header to IRS page, fixed ECL FWD/ECL BACK/ECL SELECT.
    
    * CHKL OVRD currently not working

--- salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/OPTIONS/B747_8_FMC_IrsStatus.js
+++ salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/OPTIONS/B747_8_FMC_IrsStatus.js
@@ -18,7 +18,7 @@ class FMCSaltyOptions_IrsStatus {
             ["", ""],
             ["<ECL BACK", "ECL FWD>"],
             ["", ""],
-            ["", "ECL SELECT>"],
+            ["<NEXT CHKL", "ECL SELECT>"],
             ["\xa0RETURN TO", ""],
             ["<OPTIONS", ""]
         ]);
@@ -38,20 +38,35 @@ class FMCSaltyOptions_IrsStatus {
                fmc.showErrorMessage("IRS KNOBS OFF");
            }
         }
-
-        fmc.onRightInput[4] = () => { let cursorIndex = SimVar.GetSimVarValue("L:SALTY_ECL_CURSOR_INDEX", "Enum");
+        /* LSK 4 */
+        fmc.onRightInput[3] = () => { let cursorIndex = SimVar.GetSimVarValue("L:SALTY_ECL_CURSOR_INDEX", "Enum");
                                         cursorIndex++;
                                         SimVar.SetSimVarValue("L:SALTY_ECL_CURSOR_INDEX", "Enum", cursorIndex);
                                         SimVar.SetSimVarValue("L:SALTY_ECL_CURSOR_INDEX_INC", "bool", 1);
                                     };
            
-        fmc.onLeftInput[4] = () =>  { let cursorIndex = SimVar.GetSimVarValue("L:SALTY_ECL_CURSOR_INDEX", "Enum");
+        fmc.onLeftInput[3] = () =>  { let cursorIndex = SimVar.GetSimVarValue("L:SALTY_ECL_CURSOR_INDEX", "Enum");
                                         cursorIndex--;
                                         SimVar.SetSimVarValue("L:SALTY_ECL_CURSOR_INDEX", "Enum", cursorIndex);
                                         SimVar.SetSimVarValue("L:SALTY_ECL_CURSOR_INDEX_DEC", "bool", 1);
-                                    };
-
-        fmc.onRightInput[5] = () => { SimVar.SetSimVarValue("L:SALTY_ECL_BTN", "bool", 1); };
+        };
+        /* LSK 5 */
+        fmc.onLeftInput[4] = () => {
+            if (_event == "EICAS_CHANGE_PAGE_chkl") {
+                let eclToDraw = B747_8_EICAS.sequenceElectronicChecklist();
+                if (B747_8_EICAS.currentPage !== `CHKL-${eclToDraw}`) {
+                    B747_8_EICAS.changePage(`CHKL-${eclToDraw}`);
+                    B747_8_EICAS.currentPage = `CHKL-${eclToDraw}`;
+                }
+            } else if (B747_8_EICAS.currentPage !== _event) {
+                B747_8_EICAS.currentPage = _event;
+            } else {
+                B747_8_EICAS.changePage("BLANK");
+                B747_8_EICAS.currentPage = "blank";
+                return;
+            }
+        }
+        fmc.onRightInput[4] = () => { SimVar.SetSimVarValue("L:SALTY_ECL_BTN", "bool", 1); };
 
         /* LSK6 */
         fmc.onLeftInput[5] = () => {
