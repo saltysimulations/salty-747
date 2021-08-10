c66be45fbd0c9c63c2e8479e62aeded914d6f9f5
747-4EVER <ashleytt94@hotmail.co.uk>
2021-08-10 21:05:09 +0100

    Added Comments

--- salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/EICAS/Pages/B747_8_LowerEICAS_ECL.js
+++ salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/EICAS/Pages/B747_8_LowerEICAS_ECL.js
@@ -11,14 +11,16 @@ var B747_8_LowerEICAS_ECL;
             TemplateElement.call(this, this.init.bind(this));
         }
         init() {
+            this.checklistItems = [];
             this.normalChecklistSequence = 0;
             this.cursorPosition = 0;
             this.cursor = document.querySelector("#maincursor");
             this.maxCursorIndex = this.getMaxCursorIndex();
             this.buildChecklist();
-            this.isInitialised = true;
             this.eventTimeout = 0;
+            this.isInitialised = true;
         }
+        //Event Handler for Cursor Control.
         onEvent(_event) {
             super.onEvent(_event);
             setTimeout(() => {
@@ -28,26 +30,62 @@ var B747_8_LowerEICAS_ECL;
                 if (_event === "ECL_KNOB_FWD" && this.cursorPosition < this.maxCursorIndex - 1) {
                     this.cursorPosition ++;
                     this.updateCursor();
-                    console.log("ECL FWD!" + this.cursorPosition + " " + this.maxCursorIndex);
                 } 
                 else if (_event === "ECL_KNOB_BACK" && this.cursorPosition != 0) {
                     this.cursorPosition --;
                     this.updateCursor();
-                    console.log("ECL BACK!");
                 }
                 else if (_event === "ECL_SEL_PUSH") {
-                    console.log("ECL PUSHED!");
+                    let cursorTarget = this.getCursorTarget()
+                    if (cursorTarget != -1) {
+                        this.toggleItem(cursorTarget);
+                    }
                 }
                 this.eventTimeout = 1;
             }
         }
+        //Main update loop controls UI elements based on checklist item completion state.
         update(_deltaTime) {
-
+            this.updateClosedLoopItems();
+            for (let i = 0; i < normalChecklists[this.normalChecklistSequence].items.length; i++) {
+                let text = document.querySelector("#item" + i + "-text");
+                let tick = document.querySelector("#tick" + i);
+                if (this.checklistItems[i].status == "checked") {
+                    text.style.fill = "lime";
+                    tick.style.visibility = "visible";
+                }
+                else {
+                    text.style.fill = "white";
+                    tick.style.visibility = "hidden";
+                }
+            }
         }
+        //Iterates through current closed loop checklist items and updates state to match if Simvar conditions met or not.
+        updateClosedLoopItems() {
+            let conditionsSatCounter = [];
+            let conditionsTargetNum = [];
+            for (let i = 0; i < this.checklistItems.length; i++) {
+                conditionsSatCounter[i] = 0;
+                if (this.checklistItems[i].conditionType === "closed") {
+                    for (let j = 0; j < this.checklistItems[i].conditions.length; j++) {
+                        conditionsTargetNum[i] = this.checklistItems[i].conditions.length;
+                        if (SimVar.GetSimVarValue(this.checklistItems[i].conditions[j].simvar, this.checklistItems[i].conditions[j].simvarType) === this.checklistItems[i].conditions[j].simvarTrueCondition) {
+                            conditionsSatCounter[i] ++;
+                        }
+                        if (conditionsSatCounter[i] === conditionsTargetNum[i]) {
+                            this.checklistItems[i].status = "checked";
+                        }
+                        else {
+                            this.checklistItems[i].status = "pending";
+                        }
+                    }
+                }
+            }
+        }
+        //Updates position and size of cursor UI elements.
         updateCursor() {
             this.cursor.setAttribute("x", cursorMap[this.cursorPosition * 2]);
             this.cursor.setAttribute("y", cursorMap[(this.cursorPosition * 2) + 1]);
-            console.log("x:" + cursorMap[this.cursorPosition * 2] + " y:" + cursorMap[(this.cursorPosition * 2) + 1])
             if (this.cursorPosition > -1 && this.cursorPosition < 3) {
                 this.cursor.setAttribute("width", cursorSizes.largeBox.width + "px");
                 this.cursor.setAttribute("height", cursorSizes.largeBox.height + "px");
@@ -61,36 +99,62 @@ var B747_8_LowerEICAS_ECL;
                 this.cursor.setAttribute("height", cursorSizes.items.height + "px");
             }
         }
+        //Clears display elements and main logic array.
         clearChecklist() {
             for (let i = 0; i < 8; i++) {
                 let text = this.querySelector("#item" + i + "-text");
                 let box = this.querySelector("#box" + i);
+                let tick = this.querySelector("#box" + i);
                 text.style.visibility = "hidden";
                 box.style.visibility = "hidden";
+                tick.style.visibility = "hidden";
             }
+            this.checklistItems = [];
         }
+        //Main checklist build function.
         buildChecklist() {
             this.buildCursorMap();
             this.clearChecklist();
             this.buildTitle();
             this.buildBody();
         }
+        //Builds Checklist Title.
         buildTitle() {
             let title = this.querySelector("#title-text");
             title.textContent = "\u00BB" + normalChecklists[this.normalChecklistSequence].checklistTitle + "\u00AB";
         }
+        //Iterates through each item on checklist and calls the build for each.
         buildBody() {
             for (let i = 0; i < normalChecklists[this.normalChecklistSequence].items.length; i++) {
                 this.buildItem(normalChecklists[this.normalChecklistSequence].items[i], i);
             }
         }
+        //Builds initial state and UI elements for individual checklist items.
         buildItem(item, i) {
+            //Set Item state Properties and push to main logic array checklistItems.
+            let lineItem = {};
+            lineItem.status = "pending";
+            lineItem.conditionType = item.conditionType;
+            lineItem.conditions = [];
+            if (lineItem.conditionType === "closed") {
+                for (let j = 0; j < item.conditions.length; j++) {
+                    lineItem.conditions[j] = item.conditions[j];
+                }
+            }
+            else {
+                lineItem.conditions[0] = "";
+            }
+            this.checklistItems.push(lineItem);
+
+            //Create Item text.
             let text = this.querySelector("#item" + i + "-text");
-            let box = this.querySelector("#box" + i);
             text.setAttribute("x", "11%");
             text.setAttribute("y", item.y);
             text.textContent = item.name;
             text.style.visibility = "visible";
+
+            //Create Item grey box if closed loop.
+            let box = this.querySelector("#box" + i);
             if (item.conditionType === "open") {
                 box.setAttribute("x", "2%");
                 box.setAttribute("y", item.y - 27);
@@ -99,7 +163,11 @@ var B747_8_LowerEICAS_ECL;
             else {
                 box.style.visibility = "hidden";
             }
+            //Create Item tick.
+            let tick = this.querySelector("#tick" + i);
+            tick.setAttribute("d", "M 20," + (item.y - 5) + " l 8,10 l 16,-20 l -2,-2 l -14.4,18 l -6,-8 Z");
         }
+        //Builds an array from the currently loaded checklist of valid x/y coord positions for the cursor.
         buildCursorMap() {
             let itemsCoords = [];
             for (let i = 0; i < normalChecklists[this.normalChecklistSequence].items.length; i++) {
@@ -108,13 +176,31 @@ var B747_8_LowerEICAS_ECL;
             for (let i = 0; i < normalChecklists[this.normalChecklistSequence].items.length; i++) {
                 itemsCoords.splice(i * 2 + 1, 0, normalChecklists[this.normalChecklistSequence].items[i].y - 28);
             }
-            cursorMap = cursorPosTop.concat(itemsCoords.concat(cursorPosBottom));
-            for (let i = 0; i < cursorMap.length; i++) {
-                console.log(cursorMap[i]);
-            }           
+            cursorMap = cursorPosTop.concat(itemsCoords.concat(cursorPosBottom));     
         }
+        //Gets upper bound for cursor scrolling.
         getMaxCursorIndex() {
             return 3 + normalChecklists[this.normalChecklistSequence].itemCount + 6;
+        }
+        //Gets value corresponding to currently selected checklist line item.
+        getCursorTarget() {
+            if (this.cursorPosition > 2 && this.cursorPosition < this.maxCursorIndex - 6) {
+                return this.cursorPosition - 3;
+            }
+            else {
+                return -1;
+            }
+        }
+        //Toggles open loop items between pending and checked states.
+        toggleItem(cPos) {
+            if (this.checklistItems[cPos].conditionType === "open") {
+                if (this.checklistItems[cPos].status == "pending") {
+                    this.checklistItems[cPos].status = "checked";
+                }
+                else {
+                    this.checklistItems[cPos].status = "pending";
+                }
+            }
         }
     }B747_8_LowerEICAS_ECL.Display = Display;
 })(B747_8_LowerEICAS_ECL || (B747_8_LowerEICAS_ECL = {}));
