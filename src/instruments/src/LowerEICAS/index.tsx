58bf807b02e0f50d7cbb462206c6a08d1ec95cba
747-4EVER <71572892+747-4EVER@users.noreply.github.com>
2021-10-25 19:08:58 +0900

    Added basic ECS Synoptic

---
+++ src/instruments/src/LowerEICAS/index.tsx
@@ -0,0 +1,47 @@
+/**
+ * Salty 74S
+ * Copyright (C) 2021 Salty Simulations and its contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, { FC } from "react";
+import { render } from "../Common";
+import { useSimVar } from "react-msfs";
+
+import { ECS } from "./ECS/ECS";
+
+import "./index.scss";
+import "../Common/pixels.scss";
+
+type BlackOutlineWhiteLineProps = { d: string; blackStroke?: number; whiteStroke?: number; color?: string };
+export const BlackOutlineWhiteLine: FC<BlackOutlineWhiteLineProps> = ({ d, blackStroke = 4, whiteStroke = 3, color = "white" }) => (
+    <>
+        <path stroke="black" strokeWidth={blackStroke} strokeLinecap="round" d={d}></path>
+        <path stroke={color} strokeWidth={whiteStroke} strokeLinecap="round" d={d}></path>
+    </>
+);
+
+const LowerEICAS: FC = () => {
+    return (
+        <>
+            <div className="LcdOverlay" style={{ opacity: "0.2" }} />
+            <svg className="pfd-svg" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg"> 
+            <ECS/>
+            </svg>
+        </>
+    );
+};
+
+render(<LowerEICAS />);
