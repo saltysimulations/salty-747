9c25e3a0b29bffd5c87270d12095e418211b18f8
747-4EVER <ashleytt94@hotmail.co.uk>
2022-01-24 21:50:29 +0000

    feat(PFD): Added PLI

--- src/instruments/src/PFD/AttitudeIndicator/PitchLimitIndicator.tsx
+++ src/instruments/src/PFD/AttitudeIndicator/PitchLimitIndicator.tsx
@@ -24,15 +24,37 @@ export const PLI: FC = () => {
     const [airspeed] = useSimVar("AIRSPEED INDICATED", "knots");
     const [manSpeed] = useSimVar("L:SALTY_MANEUVERING_SPEED", "knots");
     const [selectedFlaps] = useSimVar("FLAPS HANDLE INDEX", "number");
-    const [onGround] = useSimVar("RADIO HEIGHT", "feet");
+    const [onGround] = useSimVar("PLANE ALT ABOVE GROUND MINUS CG", "feet");
     const [pitch] = useSimVar("PLANE PITCH DEGREES", "degrees");
     const [pliPitch] = useSimVar("L:74S_PLI_ANGLE", "degrees");
+    const [alpha] = useSimVar("INCIDENCE ALPHA", "degrees");
 
     const getIsPliOn = (airspeed: number, manSpeed: number, flapsHandle: number, onGround: number): boolean => {
-        if ((airspeed < manSpeed || flapsHandle != 0) && onGround < 10) {
+        if ((airspeed < manSpeed || flapsHandle != 0) && onGround > 10) {
             return true;
         }
         return false;
+    };
+
+    const getPLIPitch = (flapsHandle: number, alpha: number): number => {
+        switch (flapsHandle) {
+            case 0:
+                //PLACE HOLDER FOR NOW - NEEDS AIRSPEED BASED LOGIC FOR FLAPS UP PLI
+                return 4.0 - alpha;
+            case 1:
+                return 4.0 - alpha;
+            case 2:
+                return 6.0 - alpha;
+            case 3:
+                return 7.7 - alpha;
+            case 4:
+                return 6.7 - alpha;
+            case 5:
+                return 6.0 - alpha;
+            case 6:
+                return 5.1 - alpha;
+        }
+        return 0;
     };
 
     const degreesToPixels = (angle: number): number => (angle < 0 ? Math.max(angle * 8, -12 * 8) : Math.min(angle * 8, 12 * 8));
@@ -40,7 +62,7 @@ export const PLI: FC = () => {
     return (
         <g>
             {/* PLI Bars */}
-            <g transform={`translate(0 ${degreesToPixels(Math.max(pliPitch, -30) - pitch) || 0})`} visibility={getIsPliOn(airspeed, manSpeed, selectedFlaps, onGround) ? "visible" : "visible"}>
+            <g transform={`translate(0 ${degreesToPixels(Math.max(-1 * getPLIPitch(selectedFlaps, alpha), -30) - 0) || 0})`} visibility={getIsPliOn(airspeed, manSpeed, selectedFlaps, onGround) ? "visible" : "hidden"}>
                 <path className="black-outline" d="M416 382, h33, m 0 0, h-8, l9 -14, m-9 14, m-9 0, l9 -14, m-18 14, l9 -14, m-17 14, v10" fill="none"/>
                 <path className="amber-line" d="M416 382, h33, m 0 0, h-8, l9 -14, m-9 14, m-9 0, l9 -14, m-18 14, l9 -14, m-17 14, v10" fill="none"/>
                 <path className="black-outline" d="M282 382, h-33, m 0 0, h8, l-9 -14, m9 14, m9 0, l-9 -14, m18 14, l-9 -14, m17 14, v10" fill="none"/>
