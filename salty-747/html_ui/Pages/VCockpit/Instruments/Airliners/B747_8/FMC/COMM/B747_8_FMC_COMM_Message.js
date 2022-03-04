68e3cba37e1b12bc74e868ac552cd9af4afcd069
iamdumdum1234 <nguyentrangiahuy0811@gmail.com>
2022-03-04 12:39:42 +0700

    Update B747_8_FMC_COMM_Message.js
    Just a small fix to the ACARS messages when switching from page to page.

--- salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/COMM/B747_8_FMC_COMM_Message.js
+++ salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/COMM/B747_8_FMC_COMM_Message.js
@@ -43,7 +43,7 @@ class FMC_COMM_Message {
                     console.log(offset) ;
                     store.currPage = store.currPage - 1;
                 }
-                FMC_ATC_Message.ShowPage(fmc, message, offset, store);
+                FMC_COMM_Message.ShowPage(fmc, message, offset, store);
             };
             fmc.onNextPage = () => {
                 if (lines[offset + 10]) {
@@ -51,7 +51,7 @@ class FMC_COMM_Message {
                     console.log(offset) ;
                     store.currPage = store.currPage + 1;
                 }
-                FMC_ATC_Message.ShowPage(fmc, message, offset, store);
+                FMC_COMM_Message.ShowPage(fmc, message, offset, store);
             };
         }
 
