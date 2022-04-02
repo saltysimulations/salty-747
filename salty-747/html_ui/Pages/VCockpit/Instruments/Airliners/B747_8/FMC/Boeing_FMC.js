cb0f445ac96972b41645c68aaa3d59803a3ef92a
747-4EVER <71572892+747-4EVER@users.noreply.github.com>
2022-04-02 16:25:02 +0200

    feat(AP) ILS improvements (#403)
    * Initial Commit
    
    * Bug Fixes
    
    * fix(AP): fixed AFDS Inhibits
    
    * feat(AP): Enabled SPD switch
    
    * fix(AP): SPD now activates automatically on GS capture
    
    * fix(AT): Fixed Auto Idle during TO/GA
    
    * Bug Fixes

--- salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/Boeing_FMC.js
+++ salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/FMC/Boeing_FMC.js
@@ -166,7 +166,15 @@ class Boeing_FMC extends FMCMainDisplay {
             this._navModeSelector.onNavChangedEvent('FD_TOGGLE');
         }
         else if (_event.indexOf("AP_SPD") != -1) {
-
+            const altitude = Simplane.getAltitudeAboveGround();
+            if (altitude >= 400 || this.currentFlightPhase > FlightPhase.FLIGHT_PHASE_TAKEOFF) {
+                if (this._navModeSelector.currentVerticalActiveState !== VerticalNavModeState.FLC 
+                && this._navModeSelector.currentVerticalActiveState !== VerticalNavModeState.TO
+                && this._navModeSelector.currentVerticalActiveState !== VerticalNavModeState.GA
+                && !SimVar.GetSimVarValue("L:AP_VNAV_ACTIVE", "number")) {
+                    this._navModeSelector.activateSpeedMode();
+                }
+            }
         }
         else if (_event.indexOf("AP_SPEED_INTERVENTION") != -1) {
             this.toggleSpeedIntervention();
@@ -233,6 +241,11 @@ class Boeing_FMC extends FMCMainDisplay {
         }
         else if (_event.indexOf("EXEC") != -1) {
             this.onExec();
+        }
+        else if (_event.indexOf("THROTTLE_TO_GA") != -1) {
+            if (!Simplane.getIsGrounded() && !SimVar.GetSimVarValue("AUTOTHROTTLE ACTIVE", "bool")) {
+                this._navModeSelector.handleTogaChanged(true);
+            }
         }
     }
     setThrottleMode(_mode) {
