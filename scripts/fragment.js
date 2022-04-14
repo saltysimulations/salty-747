9f3a1361da9b8b4239824879590919678abe7c1a
Ninjo <ninjomcs@gmail.com>
2022-04-14 22:19:18 +0200

    build: fragmenter script (#427)

---
+++ scripts/fragment.js
@@ -0,0 +1,48 @@
+const fragmenter = require("@flybywiresim/fragmenter");
+const fs = require("fs");
+
+const execute = async () => {
+    try {
+        const result = await fragmenter.pack({
+            baseDir: "./salty-747",
+            outDir: "./build-modules",
+            modules: [
+                {
+                    name: "html_ui",
+                    sourceDir: "./html_ui",
+                },
+                {
+                    name: "ModelBehaviorDefs",
+                    sourceDir: "./ModelBehaviorDefs",
+                },
+                {
+                    name: "Textures",
+                    sourceDir: "./SimObjects/AirPlanes/Salty_B747_8i/TEXTURE",
+                },
+                {
+                    name: "Livery",
+                    sourceDir: "./SimObjects/AirPlanes/Salty_B747_8i-livery",
+                },
+                {
+                    name: "Sound",
+                    sourceDir: "./SimObjects/AirPlanes/Salty_B747_8i/sound",
+                },
+                {
+                    name: "Model",
+                    sourceDir: "./SimObjects/AirPlanes/Salty_B747_8i/model",
+                },
+                {
+                    name: "Panel",
+                    sourceDir: "./SimObjects/AirPlanes/Salty_B747_8i/panel",
+                }
+            ],
+        });
+        console.log(result);
+        console.log(fs.readFileSync("./build-modules/modules.json").toString());
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
+
+execute();
