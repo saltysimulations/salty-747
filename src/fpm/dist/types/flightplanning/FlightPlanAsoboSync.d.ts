fd4f96dd26301b5ed48bd6d315dbdae68dda4158
Ninjo <ninjomcs@gmail.com>
2022-07-23 00:03:30 +0200

    chore: remove old built files

--- src/fpm/dist/types/flightplanning/FlightPlanAsoboSync.d.ts
+++
@@ -1,9 +0,0 @@
-import { FlightPlanManager } from "./FlightPlanManager";
-/** A class for syncing a flight plan with the game */
-export declare class FlightPlanAsoboSync {
-    static fpChecksum: number;
-    static fpListenerInitialized: boolean;
-    static init(): void;
-    static LoadFromGame(fpln: FlightPlanManager): Promise<void>;
-    static SaveToGame(fpln: FlightPlanManager): Promise<void>;
-}
